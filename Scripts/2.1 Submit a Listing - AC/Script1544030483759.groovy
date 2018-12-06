d7c9e518e02c4f49dbcc3ba9ab4ead98c07e4879
jay@getventive.com <jayge@Ventive>
2018-12-05 21:30:48 -0700

    Fixed missing objects for entire test cases

--- Scripts/2.1 Submit a Listing - AC/Script1544030483759.groovy
+++ Scripts/2.1 Submit a Listing - AC/Script1544030483759.groovy
@@ -20,65 +20,64 @@ WebUI.navigateToUrl('http://porchlightdev.getventive.com/login')
 
 WebUI.maximizeWindow()
 
-WebUI.click(findTestObject('Object Repository/porchlightdev.getventive.com/2.1 Submit a Listing - AC/Page_Connect 2  PorchLight Real Est/i_Use your PorchLight email an'))
+WebUI.click(findTestObject('2.1 Submit a Listing - AC/Page_Connect 2  PorchLight Real Est/i_Use your PorchLight email an'))
 
-WebUI.setText(findTestObject('Object Repository/porchlightdev.getventive.com/2.1 Submit a Listing - AC/Page_Sign in - Google Accounts/input_getventive.com_identifie'), 
+WebUI.setText(findTestObject('2.1 Submit a Listing - AC/Page_Sign in - Google Accounts/input_getventive.com_identifie'), 
     'ventivedev')
 
-WebUI.click(findTestObject('Object Repository/porchlightdev.getventive.com/2.1 Submit a Listing - AC/Page_Sign in - Google Accounts/span_Next'))
+WebUI.click(findTestObject('2.1 Submit a Listing - AC/Page_Sign in - Google Accounts/span_Next'))
 
-WebUI.setEncryptedText(findTestObject('Object Repository/porchlightdev.getventive.com/2.1 Submit a Listing - AC/Page_Sign in - Google Accounts/input_Too many failed attempts'), 
+WebUI.setEncryptedText(findTestObject('2.1 Submit a Listing - AC/Page_Sign in - Google Accounts/input_Too many failed attempts'), 
     'yvHVlg+we4ZtOw5V4UJZaA==')
 
-WebUI.click(findTestObject('porchlightdev.getventive.com/2.1 Submit a Listing - AC/Page_Sign in - Google Accounts/span_Next (1)'))
+WebUI.click(findTestObject('2.1 Submit a Listing - AC/Page_Sign in - Google Accounts/span_Next (1)'))
 
-WebUI.click(findTestObject('Object Repository/porchlightdev.getventive.com/2.1 Submit a Listing - AC/Page_Sign in - Google Accounts/span_Allow'))
+WebUI.click(findTestObject('2.1 Submit a Listing - AC/Page_Sign in - Google Accounts/span_Allow'))
 
-WebUI.click(findTestObject('Object Repository/porchlightdev.getventive.com/2.1 Submit a Listing - AC/Page_Connect 2  PorchLight Real Est/a_Open Houses'))
+WebUI.click(findTestObject('2.1 Submit a Listing - AC/Page_Connect 2  PorchLight Real Est/a_Open Houses'))
 
-WebUI.click(findTestObject('Object Repository/porchlightdev.getventive.com/2.1 Submit a Listing - AC/Page_Connect 2  PorchLight Real Est/a_Schedule'))
+WebUI.click(findTestObject('2.1 Submit a Listing - AC/Page_Connect 2  PorchLight Real Est/a_Schedule'))
 
-WebUI.click(findTestObject('Object Repository/porchlightdev.getventive.com/2.1 Submit a Listing - AC/Page_Connect 2  PorchLight Real Est/a_Add Listing'))
+WebUI.click(findTestObject('2.1 Submit a Listing - AC/Page_Connect 2  PorchLight Real Est/a_Add Listing'))
 
-WebUI.setText(findTestObject('Object Repository/porchlightdev.getventive.com/2.1 Submit a Listing - AC/Page_Connect 2  PorchLight Real Est/input_REColorado MLS_recolorad'), 
+WebUI.setText(findTestObject('2.1 Submit a Listing - AC/Page_Connect 2  PorchLight Real Est/input_REColorado MLS_recolorad'), 
     '123456')
 
-WebUI.setText(findTestObject('Object Repository/porchlightdev.getventive.com/2.1 Submit a Listing - AC/Page_Connect 2  PorchLight Real Est/input_IRES MLS_mls_listing_id'), 
+WebUI.setText(findTestObject('2.1 Submit a Listing - AC/Page_Connect 2  PorchLight Real Est/input_IRES MLS_mls_listing_id'), 
     '12345')
 
-WebUI.setText(findTestObject('Object Repository/porchlightdev.getventive.com/2.1 Submit a Listing - AC/Page_Connect 2  PorchLight Real Est/input_IRES MLS_address'), 
-    'Automated QA AC Listing 1')
+WebUI.setText(findTestObject('2.1 Submit a Listing - AC/Page_Connect 2  PorchLight Real Est/input_IRES MLS_address'), 'Automated QA AC Listing 1')
 
-WebUI.setText(findTestObject('Object Repository/porchlightdev.getventive.com/2.1 Submit a Listing - AC/Page_Connect 2  PorchLight Real Est/input_IRES MLS_City'), 
-    'Boise')
+WebUI.setText(findTestObject('2.1 Submit a Listing - AC/Page_Connect 2  PorchLight Real Est/input_IRES MLS_City'), 'Boise')
 
-WebUI.setText(findTestObject('Object Repository/porchlightdev.getventive.com/2.1 Submit a Listing - AC/Page_Connect 2  PorchLight Real Est/input_IRES MLS_zip'), 
-    '83702')
+WebUI.setText(findTestObject('2.1 Submit a Listing - AC/Page_Connect 2  PorchLight Real Est/input_IRES MLS_zip'), '83702')
 
-WebUI.setText(findTestObject('Object Repository/porchlightdev.getventive.com/2.1 Submit a Listing - AC/Page_Connect 2  PorchLight Real Est/input_IRES MLS_sub_area'), 
-    'Ada County')
+WebUI.setText(findTestObject('2.1 Submit a Listing - AC/Page_Connect 2  PorchLight Real Est/input_IRES MLS_sub_area'), 'Ada County')
 
-WebUI.setText(findTestObject('Object Repository/porchlightdev.getventive.com/2.1 Submit a Listing - AC/Page_Connect 2  PorchLight Real Est/input_IRES MLS_active_mls_date'), 
+WebUI.setText(findTestObject('2.1 Submit a Listing - AC/Page_Connect 2  PorchLight Real Est/input_IRES MLS_active_mls_date'), 
     '12/05/2018')
 
-WebUI.click(findTestObject('porchlightdev.getventive.com/2.1 Submit a Listing - AC/Page_Connect 2  PorchLight Real Est/td_5'))
+WebUI.click(findTestObject('2.1 Submit a Listing - AC/Page_Connect 2  PorchLight Real Est/td_5'))
 
-WebUI.setText(findTestObject('Object Repository/porchlightdev.getventive.com/2.1 Submit a Listing - AC/Page_Connect 2  PorchLight Real Est/input_IRES MLS_price'), 
-    '$500,000')
+WebUI.setText(findTestObject('2.1 Submit a Listing - AC/Page_Connect 2  PorchLight Real Est/input_IRES MLS_price'), '$500,000')
 
-WebUI.click(findTestObject('Object Repository/porchlightdev.getventive.com/2.1 Submit a Listing - AC/Page_Connect 2  PorchLight Real Est/div_IRES MLS_bootstrap-tagsinp'))
+WebUI.click(findTestObject('2.1 Submit a Listing - AC/Page_Connect 2  PorchLight Real Est/div_IRES MLS_bootstrap-tagsinp'))
 
-WebUI.setText(findTestObject('Object Repository/porchlightdev.getventive.com/2.1 Submit a Listing - AC/Page_Connect 2  PorchLight Real Est/input'), 
-    'jay.upadhyay@porchlightgroup.com')
+WebUI.setText(findTestObject('2.1 Submit a Listing - AC/Page_Connect 2  PorchLight Real Est/input'), 'jay.upadhyay@porchlightgroup.com')
 
-WebUI.click(findTestObject('Object Repository/porchlightdev.getventive.com/2.1 Submit a Listing - AC/Page_Connect 2  PorchLight Real Est/a_Create'))
+WebUI.click(findTestObject('2.1 Submit a Listing - AC/Page_Connect 2  PorchLight Real Est/a_Create'))
 
 WebUI.delay(10)
 
-WebUI.setText(findTestObject('Object Repository/porchlightdev.getventive.com/2.1 Submit a Listing - AC/Page_Connect 2  PorchLight Real Est/input_Add Listing_searchlistin'), 
+WebUI.setText(findTestObject('2.1 Submit a Listing - AC/Page_Connect 2  PorchLight Real Est/input_Add Listing_searchlistin'), 
     'automated')
 
-WebUI.verifyElementText(findTestObject(null), 'Automated QA AC Listing 1 Boise, CO 83702')
+WebUI.verifyElementText(findTestObject('2.1 Submit a Listing - AC/Page_Connect 2  PorchLight Real Est/div_Automated QA AC Listing 1'), 
+    'Automated QA AC Listing 1 Boise, CO 83702')
 
 WebUI.delay(10)
+
+WebUI.closeBrowser()
+
+WebUI.acceptAlert()
 
---
+++ Test Suites/1TestSuit.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>1TestSuit</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-12-05T21:21:43</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>4b5878d3-fb3b-4258-a70b-d3ffe1c1f2cf</testSuiteGuid>
+   <testCaseLink>
+      <guid>684e0184-56b2-4d9b-b87e-0efa254edf8a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/2.1 Submit a Listing - AC</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
